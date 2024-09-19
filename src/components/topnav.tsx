427f345cfa94bc0df596c13bf0e9f5992969bf02
Mia Pernille Treland <mia.treland@gmail.com>
2024-09-19 10:21:17 +0200

    add login and signup buttons to navbar

--- src/components/topnav.tsx
+++ src/components/topnav.tsx
@@ -1,4 +1,15 @@
 import Link from "next/link";
+import {
+  SignIn,
+  SignUp,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { QuestionMarkCircleIcon } from "./ui/SVGIcons";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 
 export function TopNav() {
   return (
@@ -13,6 +24,36 @@ export function TopNav() {
           Archive
         </span>
       </Link>
+      <div className="ml-auto">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <div className="flex flex-row items-center">
+            <span className="bg-gradient-blue-to-pink from-[#70d6ff] to-[#ff70a6] bg-clip-text text-transparent">
+              <SignInButton />
+            </span>
+            <span className="mx-2">or</span>
+            <span className="bg-gradient-pink-to-orange bg-clip-text text-transparent">
+              <SignUpButton />
+            </span>
+            <span className="ml-2 hover:cursor-pointer">
+              <HoverCard>
+                <HoverCardTrigger>
+                  <QuestionMarkCircleIcon />
+                </HoverCardTrigger>
+                <HoverCardContent className="text-sm">
+                  <p>
+                    You can use this website without logging in! But by logging
+                    in, your data will be saved and accessible on any device or
+                    browser.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+            </span>
+          </div>
+        </SignedOut>
+      </div>
     </nav>
   );
 }
